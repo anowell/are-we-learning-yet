fc484287666cd3beb13ef94c0d1faa3a9d31c403
Anthony Nowell <anowell@gmail.com>
2021-03-11 23:10:43 -0800

    Porting to rust

---
+++ _plugins/scraper/src/main.rs
@@ -0,0 +1,78 @@
+use anyhow::{bail, Context, Result};
+
+mod crates;
+mod github;
+mod data;
+mod util;
+
+use github::Github;
+use crates::CratesIo;
+use data::{InputCrateInfo, GeneratedCrateInfo, override_crate_data};
+use util::{read_yaml, write_yaml};
+use url::Url;
+use std::env;
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut args = env::args();
+    let _ = args.next();
+    let path = match args.next() {
+        Some(arg) => arg,
+        None => bail!("Usage: scraper <path_to_crates_yaml>"),
+    };
+
+    let gh = Github::new()?;
+    let crates_io = CratesIo::new()?;
+
+    let input: Vec<InputCrateInfo> = read_yaml(&path).with_context(|| format!("Error reading {}", path))?;
+    let mut generated = Vec::new();
+    for krate in input {
+        if let Some(name) = &krate.name {
+            println!("Processing crate {}", name);
+        } else if let Some(repo) = &krate.repository {
+            println!("Processing repo {}", repo);
+        } else {
+            println!("Invalid entry: {:#?}", krate);
+            continue;
+        }
+
+        let mut gen = GeneratedCrateInfo::from(&krate);
+        if let Some(crate_name) = &krate.name {
+            match crates_io.get_crate_data(crate_name).await {
+                Ok(mut data) => {
+                    override_crate_data(&mut data, &krate);
+                    gen.krate = Some(data);                            
+                }
+                Err(err) => {
+                    eprintln!("Error getting crate data for {} - {}", crate_name, err);
+                }
+            }
+        }
+
+        // Yes, we serialized in `override_crate_data` and then re-parse it as a Url,
+        // but the upstream Crate type uses a String, and I still want to deserizlied data.yaml as
+        // a Url as input validation
+        let repo_opt: Option<Url> = gen.krate.as_ref().map(|k| k.repository.as_ref().map(|r| Url::parse(&r).unwrap())).flatten().clone();
+        if let Some(repo) = repo_opt {
+            if repo.host_str() == Some("github.com") {
+                // split path including both '/' and '.', because `.git` is occasionally appended to git URLs
+                let parts = repo.path().split(&['/', '.'][..]).collect::<Vec<_>>();
+                match gh.get_repo_data(parts[1], parts[2]).await {
+                    Ok(data) => { 
+                        gen.repo = Some(data)
+                    },
+                    Err(err) => { 
+                        eprintln!("Error getting Github repo data for {}/{} - {}", parts[1], parts[2], err)
+                    },
+                }
+            }
+
+        }
+        gen.update_score();
+        generated.push(gen);
+    };
+
+
+    write_yaml("_tmp/crates_generated.yaml", generated)
+}
